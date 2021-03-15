5c916444bd2027dbf3a40a466f6eff9c03959959
Evan Geurts <GeurEvan189@GoTVDSB.ca>
2021-03-15 12:10:50 -0400

    almost done

--- part 6 - loops/Program.cs
+++ part 6 - loops/Program.cs
@@ -11,6 +11,40 @@ namespace part_6___loops
         static void Main(string[] args)
         {
 
+            int score;
+            int above =0;
+            int total=0;
+            
+            
+            bool done = false;
+            while (!done)
+            {
+                Console.WriteLine("Enter a % you got on a test");
+                score = Convert.ToInt32(Console.ReadLine());
+                if (score >= 70)
+                {
+                    above += 1;
+                    
+                   
+                    
+                }
+                total += 1;
+                Console.WriteLine("do you have another score???");
+                if (Console.ReadLine().ToLower() != "yes" )
+                     done = true;
+
+
+                Console.WriteLine($"you have {above} tests above 70 out of {total} tests");
+                
+            }
+
+
+           
+
+
+            
+
+            
 
 
 
@@ -27,20 +61,7 @@ namespace part_6___loops
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-            Console.ReadLine();
+                Console.ReadLine();
         }
     }
 }
