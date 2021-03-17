87ecc521a73054c38fca0e421a78035017c62b04
Evan Geurts <GeurEvan189@GoTVDSB.ca>
2021-03-17 09:21:03 -0400

    done randome numbers

--- part 6 - loops/Program.cs
+++ part 6 - loops/Program.cs
@@ -10,13 +10,22 @@ namespace part_6___loops
     {
         static void Main(string[] args)
         {
+            Random generator = new Random();
 
             int score;
             int above =0;
             int total=0;
-            
-            
             bool done = false;
+
+            int input;
+            int x= 0;
+            int answer = 0;
+
+            int min;
+            int max;
+            int ran;
+
+
             while (!done)
             {
                 Console.WriteLine("Enter a % you got on a test");
@@ -34,6 +43,27 @@ namespace part_6___loops
             Console.WriteLine($"{((double)above / total) *100} % of your tests are over 70%");
             Console.WriteLine("__________________________________________________________________________________________________");
 
+            Console.WriteLine("Please enter a number:");
+            input = Convert.ToInt32(Console.ReadLine());
+
+            for (x=1; x<= input; x+= 2)
+            {
+                answer +=x;
+            }
+            Console.WriteLine($"If you add all the odd numbers from 1 to {input} is {answer}");
+
+
+            Console.WriteLine("__________________________________________________________________________________________________________");
+            Console.WriteLine("Please enter a minimum:");
+            min = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Now enter a maximum");
+            max = Convert.ToInt32(Console.ReadLine());
+            for (int i =1; i <=25; i++)
+            {
+                ran = generator.Next(min, max);
+                Console.WriteLine("");
+                Console.WriteLine(ran);
+            }
 
 
 
@@ -45,18 +75,7 @@ namespace part_6___loops
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-            Console.ReadLine();
+                Console.ReadLine();
         }
     }
 }
